e1fdc5681fdc2c4eb7a583225f8e70325138769b
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-06-07 20:31:40 +0300

    add styles

--- src/pages/ShopsPage/ShopsPage.jsx
+++ src/pages/ShopsPage/ShopsPage.jsx
@@ -3,7 +3,16 @@ import styles from './ShopsPage.module.scss';
 const ShopsPage = () => {
   return (
     <div className={styles.section}>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <div className={styles.shopsContainer}>
+          <ul>
+            <li>Pizza</li>
+            <li>Sushi</li>
+            <li>Desserts</li>
+          </ul>
+        </div>
+        <div className={styles.cardsContainer}></div>
+      </div>
     </div>
   );
 };
